4147ab36e99cdf7f161e3689b4d0a381a8971c2d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-08 20:22:01 +0700

    feat: config image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["localhost"],
+  },
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/dashboard",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
