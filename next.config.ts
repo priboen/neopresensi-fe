b35f190f80939dfb5123e630f00c9ade5d6b210f
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-09 01:14:15 +0700

    feat: change image config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost"],
+    remotePatterns: [
+      {
+        hostname: "localhost",
+      },
+    ],
   },
   redirects: async () => [
     {
