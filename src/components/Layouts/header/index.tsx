cfe05f4ac48a1712f586fcfa8a8e4be9c4c5c2bd
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-08 20:25:36 +0700

    feat: setting header

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useSidebarContext } from "../sidebar/sidebar-context";
 import { MenuIcon } from "./icons";
-import { Notification } from "./notification";
 import { ThemeToggleSwitch } from "./theme-toggle";
 import { UserInfo } from "./user-info";
 
@@ -38,7 +37,7 @@ export function Header() {
         <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
           Dashboard
         </h1>
-        <p className="font-medium">Next.js Admin Dashboard Solution</p>
+        <p className="font-medium">Statistik Presensi Guru</p>
       </div>
 
       <div className="flex flex-1 items-center justify-end gap-2 min-[375px]:gap-4">
@@ -53,8 +52,6 @@ export function Header() {
         </div>
 
         <ThemeToggleSwitch />
-
-        <Notification />
 
         <div className="shrink-0">
           <UserInfo />
