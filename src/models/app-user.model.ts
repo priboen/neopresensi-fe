aeb4aa4dbea93306c0458a809e52576da6a6e8b1
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-09 01:14:52 +0700

    feat: add user model

---
+++ src/models/app-user.model.ts
@@ -0,0 +1,21 @@
+export class AppUser {
+  constructor(
+    public uuid: string,
+    public name: string,
+    public username: string,
+    public email: string,
+    public role: string,
+    public photo_url: string | null
+  ) {}
+
+  static fromJson(data: any): AppUser {
+    return new AppUser(
+      data.uuid,
+      data.name,
+      data.username,
+      data.email,
+      data.role,
+      data.photo_url
+    );
+  }
+}
