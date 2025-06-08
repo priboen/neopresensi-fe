3e09da7ad0c09d81b63ebb585ae8a19180008928
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-08 20:23:45 +0700

    feat: change features in sidebar

--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -1,102 +1,48 @@
+import { title } from "process";
 import * as Icons from "../icons";
 
 export const NAV_DATA = [
   {
-    label: "MAIN MENU",
+    label: "FITUR UTAMA",
     items: [
       {
         title: "Dashboard",
         icon: Icons.HomeIcon,
-        items: [
-          {
-            title: "eCommerce",
-            url: "/",
-          },
-        ],
-      },
-      {
-        title: "Calendar",
-        url: "/calendar",
-        icon: Icons.Calendar,
         items: [],
       },
       {
-        title: "Profile",
-        url: "/profile",
+        title: "Kehadiran",
+        url: "/attendance",
+        icon: Icons.Calendar,
+        items: [
+          { title: "Data Kehadiran", url: "/attendance/data" },
+          { title: "Data Perizinan", url: "/attendance/permission" },
+          { title: "QR Absen", url: "/attendance/qr" },
+          { title: "Jam Kerja", url: "/attendance/setting" },
+        ],
+      },
+      {
+        title: "Pengguna",
+        url: "/user",
         icon: Icons.User,
         items: [],
-      },
-      {
-        title: "Forms",
-        icon: Icons.Alphabet,
-        items: [
-          {
-            title: "Form Elements",
-            url: "/forms/form-elements",
-          },
-          {
-            title: "Form Layout",
-            url: "/forms/form-layout",
-          },
-        ],
-      },
-      {
-        title: "Tables",
-        url: "/tables",
-        icon: Icons.Table,
-        items: [
-          {
-            title: "Tables",
-            url: "/tables",
-          },
-        ],
-      },
-      {
-        title: "Pages",
-        icon: Icons.Alphabet,
-        items: [
-          {
-            title: "Settings",
-            url: "/pages/settings",
-          },
-        ],
       },
     ],
   },
   {
-    label: "OTHERS",
+    label: "FITUR TAMBAHAN",
     items: [
       {
-        title: "Charts",
+        title: "CCTV",
         icon: Icons.PieChart,
         items: [
           {
-            title: "Basic Chart",
-            url: "/charts/basic-chart",
-          },
-        ],
-      },
-      {
-        title: "UI Elements",
-        icon: Icons.FourCircle,
-        items: [
-          {
-            title: "Alerts",
-            url: "/ui-elements/alerts",
+            title: "Streaming CCTV",
+            url: "/cctv/streaming",
           },
           {
-            title: "Buttons",
-            url: "/ui-elements/buttons",
-          },
-        ],
-      },
-      {
-        title: "Authentication",
-        icon: Icons.Authentication,
-        items: [
-          {
-            title: "Sign In",
-            url: "/auth/sign-in",
+            title: "Setting CCTV",
+            url: "/cctv/streaming",
           },
         ],
       },
