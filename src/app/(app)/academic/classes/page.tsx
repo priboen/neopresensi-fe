1629c071d019c4a8c330e0bf5c7d985db5818e1a
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-18 15:40:47 +0700

    feat: initialize data

---
+++ src/app/(app)/academic/classes/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import { SkeletonTable } from "@/components/Tables/skeleton";
+import { Classes } from "@/models/classes.model";
+import { authFetch } from "@/utils/auth-fetch";
+import { useEffect, useState } from "react";
+import ClassesTable from "./_components/ClassesTable";
+
+const API_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/classes`;
+
+export default function ClassesPage() {
+  const [classes, setClasses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const fetchClasses = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const res = await authFetch(API_URL);
+      const json = await res.json();
+      if (!res.ok || json.statusCode !== 200) {
+        throw new Error(json.message);
+      }
+      const mapped = json.data.map(Classes.fromJson);
+      setClasses(mapped);
+    } catch (err) {
+      console.error("Fetch error:", err);
+      setError("Terjadi kesalahan saat memuat data.");
+      setClasses([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchClasses();
+  }, []);
+  return (
+    <>
+      <Breadcrumb pageName="Data Seluruh Kelas" />
+      <div className="space-y-10">
+        {error && (
+          <div className="rounded-md bg-red-500 text-white p-4">{error}</div>
+        )}
+        {loading ? (
+          <SkeletonTable
+            title="Data Kelas"
+            rowCount={2}
+            columns={[
+              {
+                label: "Nama Kelas",
+                align: "left",
+              },
+              {
+                label: "Aksi",
+                align: "left",
+              },
+            ]}
+          />
+        ) : classes.length === 0 ? (
+          <div className="rounded-md bg-white p-6 text-center text-base text-dark dark:bg-gray-dark dark:text-white shadow-md">
+            Tidak ada data kelas yang ditemukan.
+          </div>
+        ) : (
+          <ClassesTable classes={classes} onRefresh={fetchClasses} />
+        )}
+      </div>
+    </>
+  );
+}
